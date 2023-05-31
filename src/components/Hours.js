5c3a5a1cadb4d76c9f742df7420dd0b5b95bafac
Pawan Kumar Siwakoti <pksiwako@deakin.edu.au>
2023-05-31 22:45:10 +1000

    react not to do list

---
+++ src/components/Hours.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hours = ({ttl}) => {
+  return (
+    <div>
+       <p>You could have saved = {ttl} hr</p>
+          {/* total hours  */}
+          <div>
+            Total hour per week allocated = {}hr
+          </div>
+    </div>
+  )
+}
+
+export default Hours
